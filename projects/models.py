fd2479897d30fe5acdf6648e8a1bda70964f515c
divyam2605 <104719264+divyam2605@users.noreply.github.com>
2023-02-12 18:33:57 +0530

    Add files via upload

---
+++ projects/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    featured_image = models.ImageField(null=True, blank=True, default='default.jpg')
+    demo_link = models.CharField(max_length=1000, null=True, blank=True)
+    source_link = models.CharField(max_length=1000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    created =  models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def imageURL(self):
+        try:
+            img = self.featured_image.url
+        except:
+            img = ''
+
+        return img
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
